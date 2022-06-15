a3e8b400f6fbd909af74fa21d63d587cbb1e6e07
ortho <ahmedeyaa49@gmail.com>
2022-06-15 11:48:34 +0200

    new calCulation

--- src/test/java/Update.java
+++ src/test/java/Update.java
@@ -1,8 +1,14 @@
 public class Update {
 
     private static int x = 5;
+    private static int y = 5;
 
     public static void main(String args[]){
         System.out.println(x);
+        System.out.println(y);
+        System.out.println(x+y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(x-y);
     }
 }
